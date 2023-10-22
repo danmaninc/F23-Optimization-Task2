c89cc4421ffe425c933982dca21075a0cbfac628
Nickolaus-899 <n.petukhov@innopolis.university>
2023-10-22 11:33:56 +0300

    Interior output upd

--- Interior.h
+++ Interior.h
@@ -8,7 +8,27 @@
 #include "Matrix.h"
 
 struct Interior {
+    double z;
+    int accuracy;
+    std::vector<double> variables;
 
+    Interior(const std::size_t size, const int eps) {
+        z = 0;
+        accuracy = eps;
+        variables.resize(size, 0);
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, Interior& answer) {
+        out << "Value of z: " << answer.z << std::endl;
+
+        for (int i = 0; i < answer.variables.size(); ++i)
+            out << std::setprecision(answer.accuracy)
+                << std::fixed
+                << "x" << i + 1 << " = " << answer.variables[i] << std::endl;
+
+        out << std::endl;
+        return out;
+    }
 };
 
 #endif //F23_OPTIMIZATION_TASK2_INTERIOR_H
